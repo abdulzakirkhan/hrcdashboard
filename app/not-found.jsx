a02ad0904719560a386e7c906f8ce36bcd87c796
Abdul Zakir <abdul.zakir@egeeksglobal.com>
2025-07-17 18:00:38 +0500

    added not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+"use client"
+export default function NotFound() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-gray-500 mt-2">The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
